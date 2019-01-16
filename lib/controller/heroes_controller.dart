e928b5a2a3362b5225b010e1706320c7c9bf46c0
Modern Coding <cormorant.programming@gmail.com>
2019-01-16 11:08:44 +0700

    CRUD complete

--- lib/controller/heroes_controller.dart
+++ lib/controller/heroes_controller.dart
@@ -26,11 +26,12 @@ class HeroesController extends ResourceController {
 
   @Operation.get()
   Future<Response> getAllHeroes({@Bind.query('name') String name}) async {
-    final heroQuery = Query<Hero>(context);
+    final Query<Hero> heroQuery = Query<Hero>(context);
+
     if (name != null) {
       heroQuery.where((h) => h.name).contains(name, caseSensitive: false);
     }
-    final heroes = await heroQuery.fetch();
+    final List<Hero> heroes = await heroQuery.fetch();
 
     return Response.ok(heroes);
   }
@@ -38,14 +39,61 @@ class HeroesController extends ResourceController {
 
   @Operation.get('id')
   Future<Response> getHeroByID(@Bind.path('id') int id) async {
-    final heroQuery = Query<Hero>(context)
+    final Query<Hero> heroQuery = Query<Hero>(context)
       ..where((h) => h.id).equalTo(id);    
 
-    final hero = await heroQuery.fetchOne();
+    final Hero hero = await heroQuery.fetchOne();
 
     if (hero == null) {
       return Response.notFound();
     }
+
     return Response.ok(hero);
+  }
+
+
+  @Operation.post()
+  Future<Response> createHero(@Bind.body() Hero inputHero) async {
+    final Query<Hero> query = Query<Hero>(context)
+      ..values = inputHero;
+
+    final Hero insertedHero = await query.insert();
+
+    return Response.ok(insertedHero);
+  }
+
+
+  @Operation.put('id')
+  Future<Response> updateRead(
+    @Bind.body() Hero updateHero,
+    @Bind.path('id') int id
+  ) async {
+
+    final Query<Hero> query = Query<Hero>(context)
+      ..values = updateHero
+      ..where((h) => h.id).equalTo(id);
+
+    final Hero updatedHero = await query.updateOne();
+
+    if(updatedHero == null){
+      return Response.notFound(body: 'Item not found');
+    }
+
+    return Response.ok(updatedHero);
+  }
+
+
+  @Operation.delete('id')
+  Future<Response> deleteHero(@Bind.path('id') int id) async {
+    final Query<Hero> query = Query<Hero>(context)
+      ..where((h) => h.id).equalTo(id);
+
+    final int deletedCount = await query.delete();
+
+    if(deletedCount == 0){
+      return Response.notFound(body: 'Item not found');
+    }
+
+    return Response.ok('Deleted $deletedCount item(s)');
   }
 }
